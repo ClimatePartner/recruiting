2283cb0dca9394452398cddaf02ace6018186295
Matthew Jordan <matthew.jordan@climatepartner.com>
2021-08-16 11:39:46 +0200

    feat: add initial js test template

---
+++ technical-interviews/javascript/carbon-offset-projects/carbon-offset-projects-test-template.js
@@ -0,0 +1,22 @@
+import OffsetProjects from "./index";
+
+describe("Add", () => {
+    it("Adds a project", () => {
+        const projects = new OffsetProjects();
+
+        projects.add("Wind", "Germany", 2000);
+        projects.add("Hydropower", "Congo", 6000);
+        projects.add("Wind", "Austria", 1000);
+        projects.add("Forest", "Germany", 500);
+
+        expect(projects.getProjects());
+    });
+
+    it("Increases the amount when adding the same project", () => {});
+});
+
+describe("Remove", () => {
+    it("Removes a projects offset amount", () => {});
+
+    it("Removes an entire project when total is 0", () => {});
+});
